233ad2ac3db0cf569e0ec44eca405c95e46d9e59
Todd Griffin <todd.griffin61@outlook.com>
2025-03-26 22:25:06 +0000

    created AST

--- main.py
+++ main.py
@@ -22,4 +22,5 @@ if __name__ == '__main__':
         program: Program = parser.parse_program()
 
         with open("debug/ast.json", "w") as f:
+            print(program.json())
             json.dump(program.json(), f, indent=4)
